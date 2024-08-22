4a27dbe9a3a72fb0cad547f4cdf1f006d5eb1b00
Gianluca Bredice <bredicegian@gmail.com>
2024-08-22 14:39:20 -0300

    feature: add required enpoints for pass tests

--- index.js
+++ index.js
@@ -1,18 +1,90 @@
-const express = require('express')
+const express = require("express")
 const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const cors = require("cors")
+require("dotenv").config()
 
 app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+app.use(express.static("public"))
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
 
+const USERS = []
+const EXCERCISES = []
 
+app.post("/api/users", function (req, res) {
+  const { username } = req.body
+  const _id = (USERS.length + 1).toString()
+
+  USERS.push({ username, _id })
+
+  console.log(`${username} created!`)
+
+  return res.json({ username, _id })
+})
+
+app.get("/api/users", function (_, res) {
+  return res.json(USERS)
+})
+
+app.post("/api/users/:_id/exercises", function (req, res) {
+  const { description, duration, date } = req.body
+  const { _id } = req.params
+  const objectDate = date ? new Date(date) : new Date()
+  const user = USERS.find((user) => user._id === _id)
+
+  console.log(`/api/users/${_id}/exercises`, user)
+  if (!user) return res.status(404).json({ message: "User not found." })
+
+  const payload = {
+    user,
+    description,
+    duration: parseInt(duration),
+    date: objectDate.toDateString(),
+  }
+
+  EXCERCISES.push(payload)
+
+  return res.json(payload)
+})
+
+app.get("/api/users/:_id/logs", function (req, res) {
+  const { _id } = req.params
+  const { from, to, limit } = req.query
+  const user = USERS.find((item) => item._id === _id)
+  if (!user) return res.status(404).json({ message: "User not found." })
+
+  let logs = EXCERCISES.filter((item) => item._id === _id)
+
+  if (from)
+    logs = logs.filter(
+      (log) => new Date(from).getTime() <= new Date(log.date).getTime()
+    )
+
+  if (to)
+    logs = logs.filter(
+      (log) => new Date(log.date).getTime() < new Date(to).getTime()
+    )
+
+  if (limit) logs = logs.slice(0, parseInt(limit))
+
+  return res.json({
+    ...user,
+    count: logs.length,
+    log: logs.map((item) => ({
+      description: item.description,
+      duration: item.duration,
+      date: item.date,
+    })),
+  })
+})
+
+app.get
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+  console.log("Your app is listening on port " + listener.address().port)
 })
